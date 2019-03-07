5b34d8abe9bf80dff9538e387ca4eb32c7a68761
杨小è¾? <2781568757@qq.com>
2019-03-07 14:40:40 +0800

    init code

---
+++ src/Module/Index.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace YangXiaoHui\Module;
+/**
+ * Description of index
+ *
+ * @author Administrator
+ */
+class Index
+{
+    public static function test($arg = "module Index"){
+        return $arg;
+    }
+}
---
+++ src/Module1/Index.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace YangXiaoHui\Module1;
+
+/**
+ * Description of Index
+ *
+ * @author Administrator
+ */
+class Index
+{
+    public static function test($arg = "module1 Index"){
+        return $arg;
+    }
+}
